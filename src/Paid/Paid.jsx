2330fbdd8748b4f5e2c150debefec209ef96d5fe
yusuf099 <y76423679@gmail.com>
2024-05-30 10:36:59 +0600

    media

--- src/Paid/Paid.jsx
+++ src/Paid/Paid.jsx
@@ -1,36 +1,150 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { Modal, Button } from "@mui/material";
 import "./paid.css";
 
 export default function Paid() {
   useEffect(() => {
-    AOS.init({ duration: 1000 }); // Varsayılan animasyon süresi: 1000ms
+    AOS.init({ duration: 1000 }); // Default animation duration: 1000ms
   }, []);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [movieData, setMovieData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    fetchMovieData();
+  }, []);
+
+  const fetchMovieData = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      const data = {
+        title: "Братство (фильм, 2019)",
+        description:
+          "«Бра́тство» — российский военный фильм режиссёра Павла Лунгина, посвящённый выводу советских войск из Афганистана на завершающем этапе Афганской войны. Датой выхода в прокат в России первоначально было объявлено 6 декабря 2018 года, затем премьеру перенесли на полгода.",
+        price: "13$",
+        trailerLink: "https://youtu.be/Lp8XVPRrtTQ?t=1",
+      };
+      setMovieData(data);
+      setIsLoading(false);
+    }, 1000);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <div>
-      <section className="card__wrapper">
-        <div className="container">
-          <div className="cards">
-            <div data-aos="zoom-out" className="card"></div>
-            <div  data-aos="flip-down" className="card__info">
-              <h2>Братство (фильм, 2019)</h2>
-              <h2 className="info__text">
-                «Бра́тство» — российский военный фильм режиссёра Павла Лунгина,
-                посвящённый выводу советских войск из Афганистана на завершающем
-                этапе Афганской войны. Датой выхода в прокат в России
-                первоначально было объявлено 6 декабря 2018 года, затем премьеру
-                перенесли на полгода.
-              </h2>
-              <div className="btns">
-                <button className="custom-button">Play</button>
-                <button className="custom-button">dinahuy</button>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : movieData ? (
+        <section className="card__wrapper">
+          <div className="container">
+            <div className="cards">
+              <div data-aos="zoom-out" className="card"></div>
+              <div data-aos="flip-down" className="card__info">
+                <h2>{movieData.title}</h2>
+                <h2 className="info__text">{movieData.description}</h2>
+                <h2>Price: {movieData.price}</h2>
+
+                <div className="btns">
+                  <div className="b">
+                    <button className="custom-button" onClick={openModal}>
+                      Pay
+                    </button>
+                  </div>
+                  <a href={movieData.trailerLink}>
+                    <button className="custom-button">Trailer</button>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      ) : (
+        <div>No movie data available.</div>
+      )}
+      <PaymentModal open={isModalOpen} onClose={closeModal} />
     </div>
+  );
+}
+
+function PaymentModal({ open, onClose }) {
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+      aria-labelledby="payment-modal-title"
+      aria-describedby="payment-modal-description"
+      className="modal"
+    >
+      <div className="modals">
+        <div className="modal-overlay">
+          <div className="modal">
+            <section className="add-card page">
+              <form className="form">
+                <label htmlFor="name" className="label">
+                  <span className="title">Card holder full name</span>
+                  <input
+                    className="input-field"
+                    type="text"
+                    name="input-name"
+                    title="Input title"
+                    placeholder="Enter your full name"
+                  />
+                </label>
+                <label htmlFor="serialCardNumber" className="label">
+                  <span className="title">Card Number</span>
+                  <input
+                    id="serialCardNumber"
+                    className="input-field"
+                    type="number"
+                    name="input-name"
+                    title="Input title"
+                    placeholder="0000 0000 0000 0000"
+                  />
+                </label>
+                <div className="split">
+                  <label htmlFor="ExDate" className="label">
+                    <span className="title">Expiry Date</span>
+                    <input
+                      id="ExDate"
+                      className="input-field"
+                      type="text"
+                      name="input-name"
+                      title="Expiry Date"
+                      placeholder="01/23"
+                    />
+                  </label>
+                  <label htmlFor="cvv" className="label">
+                    <span className="title"> CVV</span>
+                    <input
+                      id="cvv"
+                      className="input-field"
+                      type="number"
+                      name="cvv"
+                      title="CVV"
+                      placeholder="CVV"
+                    />
+                  </label>
+                </div>
+                <input
+                  className="checkout-btn"
+                  type="button"
+                  value="Checkout"
+                />
+              </form>
+            </section>
+          </div>
+        </div>
+      </div>
+    </Modal>
   );
 }
--- src/components/common/PosterSlide.jsx
+++ src/components/common/PosterSlide.jsx
@@ -8,12 +8,29 @@ const PosterSlide = ({ posters }) => {
     <AutoSwiper>
       {[...posters].splice(0, 10).map((item, index) => (
         <SwiperSlide key={index}>
-          <Box sx={{
-            paddingTop: "160%",
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-            backgroundImage: `url(${tmdbConfigs.posterPath(item.file_path)})`
-          }} />
+          <Box
+            sx={{
+              paddingTop: "160%",
+              margin: "10px",
+              boxShadow:
+                "rgba(255, 255, 255, 0.5) 0px 0px 8px, rgba(0, 0, 0, 0.35) 0px 2px 15px",
+              borderRadius: "10px",
+              "&:hover": {
+                borderRadius: "10px",
+                "& .media-info": {
+                  opacity: 1,
+                  bottom: "0px",
+                  borderRadius: "10px",
+                },
+                "& .media-back-drop, & .media-play-btn": { opacity: 3 },
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+                backgroundImage: `url(${tmdbConfigs.posterPath(
+                  item.file_path
+                )})`,
+              },
+            }}
+          />
         </SwiperSlide>
       ))}
     </AutoSwiper>
